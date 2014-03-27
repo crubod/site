06c14d125ea56e4366d6e35c9a049faee3f37be7
yoku2010 <yoku2010@gmail.com>
2014-03-27 22:58:16 +0530

    Old Site Deleted | New Site Uploaded

---
+++ js/main.js
@@ -0,0 +1,150 @@
+"use strict";
+/* ==============================================
+jQuery
+=============================================== */
+$(document).ready(function () {
+	
+	// load tweets
+	//$(".follow .load-tweets").load("php/twitter.php");
+	
+	$("[href='#']").click(function(e){
+		e.preventDefault();
+	});
+	
+	// show, hide navbar
+	$(".tile").click(function(){
+		$(".navbar").animate({bottom:0},"slow");
+	}); 
+	$(".home-link").click(function(){
+		$(".navbar").animate({bottom:-50},"slow");
+	}); 
+
+	// navbar-callapse close on click
+	$('.navbar li a').on('click',function(){
+		if ( $('.navbar-collapse').hasClass("in") ) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	})
+	
+	$("#about-carousel").carousel({interval: 2000});
+	
+	// portfolio
+	$("#grid").mixitup();
+	
+	// portfolio hover
+	$("#grid li a ").each(function() { 
+		$(this).hoverdir(); 
+	});
+
+	// center box
+	function centerBox(){
+		var wHeight = $(window).height() ;
+		$(".section .center-box").each(function() {
+			var paddingTop =  $(this).height() ;
+			if ( paddingTop < wHeight ) {
+				paddingTop = ( wHeight - paddingTop ) / 2;
+				$(this).css("padding-top",paddingTop);
+			} else {
+				$(this).css("padding-top","0");
+			}
+		});
+	};
+	
+	$(window).resize(function(){
+		centerBox();
+	}).resize();
+
+	// contact form
+	$('input, textarea').placeholder();
+	
+	$('#contactform').submit(function(){
+	
+		var action = $(this).attr('action');
+		
+		$("#state-message").slideUp(750,function() {
+		$('#state-message').hide();
+		
+		$.post(action, { 
+			name: $('#name').val(),
+			email: $('#email').val(),
+			message: $('#message').val()
+		},
+			function(data){
+				document.getElementById('state-message').innerHTML = data;
+				$('#state-message').slideDown('slow');
+				$('#contactform img.loader').fadeOut('slow',function(){$(this).remove()});
+				$('#submit').removeAttr('disabled'); 
+				if(data.match('success') != null) $('#contactform').slideUp('slow');
+			}
+		);
+		});
+		return false; 
+	});
+
+});
+
+/* ==============================================
+Testimonials
+=============================================== */
+
+jQuery(function( $ ){
+	
+	var randomnumber, quoteclass, author, timeout;
+	
+	startTestimonials();
+	
+	$('.client .photos ul li').hover( function(){
+
+		window.clearTimeout(timeout);
+		
+		$('.client .photos ul li.active').removeClass('active');
+		
+		quoteclass = $(this).attr('class');
+		
+		author = $(this).find('img').attr('alt');
+		author = author.split('-');
+		author = author[0] + '<span> - ' + author[1] + '</span>';
+		
+		$('.client .quotes ul li.active').fadeOut('slow', function(){
+			$(this).removeClass('active');
+			$('.client .quotes ul li.' + quoteclass).fadeIn().addClass('active');
+			$('.client .photos .author').html(author);
+		});
+		
+		$(this).addClass('active');
+		
+	}, function(){
+		timeout = window.setTimeout( startTestimonials, 5000 );
+		return false;
+	});
+	
+	function startTestimonials() {
+		
+		$('.client .photos ul li.active').removeClass('active');
+		
+		randomnumber = Math.floor( (Math.random()*6) + 1 );
+		
+		author = $('.client .photos ul li.quote-' + randomnumber).find('img').attr('alt');
+		author = author.split('-');
+		author = author[0] + '<span> - ' + author[1] + '</span>';
+		
+		$('.client .quotes ul li.active').fadeOut('slow', function(){
+			$(this).removeClass('active');
+			$('.client .quotes ul li.quote-' + randomnumber).fadeIn().addClass('active');
+			$('.client .photos .author').html(author);
+		});
+		
+		$('.client .photos ul li.quote-' + randomnumber).addClass('active');
+		
+		timeout = window.setTimeout( startTestimonials, 5000 );
+	}
+	
+});
+
+
+/* ==============================================
+Loading
+=============================================== */
+$(window).load(function(){
+	jQuery('#loading').fadeOut(1000);
+});
